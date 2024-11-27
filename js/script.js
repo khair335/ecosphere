e5fef1f571e88b83d2d0a7765763ac20f11bdf09
khair335 <abulkhair6363@gmail.com>
2024-11-27 21:55:21 +0600

    add

--- js/script.js
+++ js/script.js
@@ -13,15 +13,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
   $(document).ready(function(){
     $('.slider').slick({
-      infinite: true,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      arrows: true,
-      dots: false,
-      autoplay: true,
-      autoplaySpeed: 4000
-
-    });
+    dots: !true,
+    infinite: false,
+      speed: 500,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
   });
 
 // Add this to your JavaScript file
@@ -40,3 +65,19 @@ document.addEventListener('DOMContentLoaded', function() {
         magic360.src = images[currentIndex];
     });
 });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.header');
+    const logo = document.querySelector('.logo');
+
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+        header.classList.add('scrolled');
+        logo.src = '/assets/icon/logo.svg';
+      } else {
+        header.classList.remove('scrolled');
+        logo.src = '/assets/icon/logoDark.svg';
+      }
+    });
+  });
